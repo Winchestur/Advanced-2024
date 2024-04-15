f90cc93eb8c67fc33a7ec69635821beb67bc61cf
Winchestur <deividyordanov@abv.bg>
2024-04-15 19:10:51 +0300

    Stacks and Queues-Lab HomeWork

---
+++ 01.Stacks and Queues - Lab/01. Reverse a String.cs
@@ -0,0 +1,20 @@
+﻿using System.Runtime.InteropServices.ComTypes;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        string text = Console.ReadLine();
+
+        Stack<char> stack = new Stack<char>(text);
+
+        string newText = "";
+
+        while (stack.Count != 0)
+        {
+            newText += stack.Pop();
+        }
+
+        Console.WriteLine(newText);
+    }
+}
---
+++ 01.Stacks and Queues - Lab/02.Stack Sum.cs
@@ -0,0 +1,61 @@
+﻿using System.Runtime.InteropServices.ComTypes;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        List<int> numbers = Console.ReadLine().Split(" ").Select(int.Parse).ToList();
+
+        Stack<int> stack = new Stack<int>(numbers);
+
+        List<string> commands = Console.ReadLine().Split(" ").ToList();
+
+        while (commands[0].ToLower() != "end")
+        {
+            string typeOfCommand = commands[0].ToLower();
+
+            switch (typeOfCommand)
+            {
+                case "add":
+                    add(stack, commands);
+                    break;
+                case "remove":
+                    remove(stack, commands);
+                    break;
+            }
+
+            commands = Console.ReadLine().Split(" ").ToList();
+        }
+
+        int result = 0;
+
+        while (stack.Count != 0)
+        {
+            result += stack.Pop();
+        }
+
+        Console.WriteLine($"Sum: {result}");
+    }
+
+    private static void remove(Stack<int> stack, List<string> commands)
+    {
+        int count = int.Parse(commands[1]);
+
+        if (count <= stack.Count)
+        {
+            for (int i = 0; i < count; i++)
+            {
+                stack.Pop();
+            }
+        }
+    }
+
+    private static void add(Stack<int> stack, List<string> commands)
+    {
+        int firstNum = int.Parse(commands[1]);
+        int secondNum = int.Parse(commands[2]);
+
+        stack.Push(firstNum);
+        stack.Push(secondNum);
+    }
+}
