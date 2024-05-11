84b29fe82e4c362ccd7749f01181290b8e467ffc
Winchestur <deividyordanov@abv.bg>
2024-05-11 11:53:31 +0300

    Stacks and Queues - Exercise HomeWork

---
+++ 02.Stacks and Queues - Exercise/11. Key Revolver.cs
@@ -0,0 +1,50 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        int pricePerBullet = int.Parse(Console.ReadLine());
+        int sizeBarrel = int.Parse(Console.ReadLine());
+        Stack<int> bullet = new Stack<int>(Console.ReadLine().Split().Select(int.Parse).ToList());
+        Queue<int> locks = new Queue<int>( Console.ReadLine().Split().Select(int.Parse).ToList());
+        int valueOfTheIntelligence = int.Parse(Console.ReadLine());
+
+        int counter = 0;
+
+        while (bullet.Any() && locks.Any())
+        {
+            counter++;
+
+            if (bullet.Peek() <= locks.Peek())
+            {
+                bullet.Pop();
+                locks.Dequeue();
+                Console.WriteLine($"Bang!");
+            }
+            else
+            {
+                bullet.Pop();
+                Console.WriteLine($"Ping!");
+            }
+
+            if (bullet.Any() && counter % sizeBarrel == 0)
+            {
+                Console.WriteLine($"Reloading!");
+            }
+        }
+
+        if (bullet.Count == 0 && locks.Count > 0)
+        {
+            int bulletCost = counter * pricePerBullet;
+            int earned = valueOfTheIntelligence - bulletCost;
+
+            Console.WriteLine($"Couldn't get through. Locks left: {locks.Count}");
+        }
+        else if (locks.Count == 0)
+        {
+            int bulletCost = counter * pricePerBullet;
+            int earned = valueOfTheIntelligence - bulletCost;
+
+            Console.WriteLine($"{bullet.Count} bullets left. Earned ${earned}");
+        }
+    }
+}
---
+++ 02.Stacks and Queues - Exercise/12. Cups and Bottles.cs
@@ -0,0 +1,48 @@
+﻿class Program
+{
+    static void Main(string[] args)
+    {
+        Queue<int> cups = new Queue<int>(Console.ReadLine().Split().Select(int.Parse));
+        Stack<int> bottles = new Stack<int>(Console.ReadLine().Split().Select(int.Parse));
+
+        int wastedWater = 0;
+
+        while (cups.Any() && bottles.Any())
+        {
+            if (cups.Peek() > bottles.Peek())
+            {
+                int result = cups.Dequeue() - bottles.Pop();
+                Queue<int> cup = new Queue<int>(cups);
+                cups.Clear();
+                cups.Enqueue(result);
+
+                for (int i = 0; i < cup.Count; i++)
+                {
+                    cups.Enqueue(cup.Dequeue());
+                    i--;
+                }
+                cup.Clear();
+            }
+            else if (cups.Peek() == bottles.Peek())
+            {
+                cups.Dequeue();
+                bottles.Pop();
+            }
+            else
+            {
+                wastedWater += bottles.Pop() - cups.Dequeue();
+            }
+        }
+
+        if (cups.Count == 0)
+        {
+            Console.WriteLine($"Bottles: {string.Join(" ", bottles)}");
+            Console.WriteLine($"Wasted litters of water: {wastedWater}");
+        }
+        else if (bottles.Count == 0)
+        {
+            Console.WriteLine($"Cups: {string.Join(" ", cups)}");
+            Console.WriteLine($"Wasted litters of water: {wastedWater}");
+        }
+    }
+}
