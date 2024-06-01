586f9f1b612fab662e1c9e096563b89165e317ec
Winchestur <deividyordanov@abv.bg>
2024-06-01 16:05:07 +0300

    Stacks and Queues - Exercise HomeWork

---
+++ 02.Stacks and Queues - Exercise/07. Truck Tour.cs
@@ -0,0 +1,37 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+Queue<List<int>> trucks = new Queue<List<int>>();
+
+for (int i = 0; i < n; i++)
+{
+    List<int> list = Console.ReadLine().Split(" ",
+        StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToList();
+
+    trucks.Enqueue(list);
+}
+int check = 0;
+int index = 0;
+int result = 0;
+
+while (true)
+{
+    result = 0;
+    foreach (var item in trucks)
+    {
+        result += item[0] - item[1];
+
+        if (result < 0)
+        {
+            trucks.Enqueue(trucks.Dequeue());
+            index++;
+            break;
+        }
+    }
+
+    if (result >= 0)
+    {
+        break;
+    }
+}
+
+Console.WriteLine(index);
