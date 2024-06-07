5978ce14409674ae37755633c671bda8fc566fc0
Winchestur <deividyordanov@abv.bg>
2024-06-07 22:58:08 +0300

    class

---
+++ Нова папка/Family.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefiningClasses
+{
+    public class Family
+    {
+        public static List<Person> Members = new List<Person>();
+
+        public static void AddMember(Person member)
+        {
+            Members.Add(member);
+        }
+
+        public static void GetOldestMember()
+        {
+            string name = "";
+            int currentOlderPerson = int.MinValue;
+            foreach (var item in Members)
+            {
+                if (currentOlderPerson < item.Age)
+                {
+                    name = item.Name;
+                    currentOlderPerson = item.Age;
+                }
+            }
+
+            Console.WriteLine($"{name} {currentOlderPerson}");
+        }
+    }
+}
---
+++ Нова папка/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            int n = int.Parse(Console.ReadLine());
+
+            for (int i = 0; i < n; i++)
+            {
+                List<string> list = Console.ReadLine().Split().ToList();
+                string name = list[0];
+                int age = int.Parse(list[1]);
+
+                Person person = new Person(name, age);
+
+                Family.AddMember(person);
+            }
+            Family.GetOldestMember();
+        }
+    }
+}
